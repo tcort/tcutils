eddfebf8f143060007feb5e5a562c6e899c23e1a
Thomas Cort <tcort@tcort.dev>
2022-03-07 10:58:35 +0000

    feat(scrub): add scrub utility

---
+++ src/scrub.c
@@ -0,0 +1,185 @@
+ /*
+    scrub -- overwrites files using the 35 pass Gutmann technique
+    Copyright (C) 2022  Thomas Cort
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <getopt.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+struct pass {
+	enum method { RANDOM, PATTERN } method;
+	uint8_t pattern[3];
+};
+
+/*
+ * Pass schedule is from:
+ *
+ * 'Secure Deletion of Data from Magnetic and Solid-State Memory'
+ *   by Peter Gutmann
+ *
+ * https://www.cs.auckland.ac.nz/~pgut001/pubs/secure_del.html
+ */
+
+static struct pass passes[35] = {
+	{ RANDOM,  { 0x00, 0x00, 0x00 } },
+	{ RANDOM,  { 0x00, 0x00, 0x00 } },
+	{ RANDOM,  { 0x00, 0x00, 0x00 } },
+	{ RANDOM,  { 0x00, 0x00, 0x00 } },
+	{ PATTERN, { 0x55, 0x55, 0x55 } }, 
+	{ PATTERN, { 0xAA, 0xAA, 0xAA } }, 
+	{ PATTERN, { 0x92, 0x49, 0x24 } }, 
+	{ PATTERN, { 0x49, 0x24, 0x92 } }, 
+	{ PATTERN, { 0x24, 0x92, 0x49 } }, 
+	{ PATTERN, { 0x00, 0x00, 0x00 } }, 
+	{ PATTERN, { 0x11, 0x11, 0x11 } }, 
+	{ PATTERN, { 0x22, 0x22, 0x22 } }, 
+	{ PATTERN, { 0x33, 0x33, 0x33 } }, 
+	{ PATTERN, { 0x44, 0x44, 0x44 } }, 
+	{ PATTERN, { 0x55, 0x55, 0x55 } }, 
+	{ PATTERN, { 0x66, 0x66, 0x66 } }, 
+	{ PATTERN, { 0x77, 0x77, 0x77 } }, 
+	{ PATTERN, { 0x88, 0x88, 0x88 } }, 
+	{ PATTERN, { 0x99, 0x99, 0x99 } }, 
+	{ PATTERN, { 0xAA, 0xAA, 0xAA } }, 
+	{ PATTERN, { 0xBB, 0xBB, 0xBB } }, 
+	{ PATTERN, { 0xCC, 0xCC, 0xCC } }, 
+	{ PATTERN, { 0xDD, 0xDD, 0xDD } }, 
+	{ PATTERN, { 0xEE, 0xEE, 0xEE } }, 
+	{ PATTERN, { 0xFF, 0xFF, 0xFF } }, 
+	{ PATTERN, { 0x92, 0x49, 0x24 } }, 
+	{ PATTERN, { 0x49, 0x24, 0x92 } }, 
+	{ PATTERN, { 0x24, 0x92, 0x49 } }, 
+	{ PATTERN, { 0x6D, 0xB6, 0xDB } },
+	{ PATTERN, { 0xB6, 0xDB, 0x6D } },
+	{ PATTERN, { 0xDB, 0x6D, 0xB6 } },
+	{ RANDOM,  { 0x00, 0x00, 0x00 } },
+	{ RANDOM,  { 0x00, 0x00, 0x00 } },
+	{ RANDOM,  { 0x00, 0x00, 0x00 } },
+	{ RANDOM,  { 0x00, 0x00, 0x00 } }
+};
+
+#define NPASSES (sizeof(passes)/sizeof(passes[0]))
+
+int main(int argc, char *argv[]) {
+
+	int ch, fd, rc, i, pati;
+	FILE *fp;
+	struct stat st;
+	off_t pos;
+
+	static struct option long_options[] = {
+		{ "help", no_argument, 0, 'h' },
+		{ "version", no_argument, 0, 'V' },
+		{ 0, 0, 0, 0 }
+	};
+
+	while ((ch = getopt_long(argc, argv, "hV", long_options, NULL)) != -1) {
+		switch (ch) {
+			case 'h':
+				fprintf(stdout, "scrub -- overwrites files using the 35 pass Gutmann technique\n");
+				fprintf(stdout, "\n");
+				fprintf(stdout, "usage: scrub [OPTIONS] FILENAME\n");
+				fprintf(stdout, "\n");
+				fprintf(stdout, "  -h, --help     print help text\n");
+			fprintf(stdout, "  -V, --version  print version and copyright info\n");
+				fprintf(stdout, "\n");
+				fprintf(stdout, "examples:\n");
+				fprintf(stdout, "\n");
+				fprintf(stdout, "  # overrite foo.txt\n");
+				fprintf(stdout, "  scrub foo.txt\n");
+				exit(EXIT_SUCCESS);
+				break;
+			case 'V':
+				fprintf(stdout, "scrub (tcutils) v1.0.0\n");
+				fprintf(stdout, "Copyright (C) 2022  Thomas Cort\n");
+				fprintf(stdout, "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n");
+				fprintf(stdout, "This is free software: you are free to change and redistribute it.\n");
+				fprintf(stdout, "There is NO WARRANTY, to the extent permitted by law.\n");
+				fprintf(stdout, "\n");
+				fprintf(stdout, "Written by Thomas Cort.\n");
+				exit(EXIT_SUCCESS);
+				break;
+			default:
+				fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
+				exit(EXIT_FAILURE);
+				break;
+		}
+
+	}
+
+	argc -= optind;
+	argv += optind;
+
+	if (argc != 1) {
+		fprintf(stderr, "usage: scrub [OPTIONS] FILENAME\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fp = fopen(argv[0], "r+");
+	if (fp == NULL) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
+
+	fd = fileno(fp);
+	if (fd == -1) {
+		perror("fileno");
+		fclose(fp);
+		exit(EXIT_FAILURE);
+	}
+
+	rc = fstat(fd, &st);
+	if (rc == -1) {
+		perror("fstat");
+		fclose(fp);
+		exit(EXIT_FAILURE);
+	}
+
+	/* if OS randomizes PIDs, then this is a random seed */
+	srand((unsigned int) getpid());
+
+	for (i = 0; i < NPASSES; i++) {
+		rewind(fp);
+		pati = 0;
+		for (pos = 0; pos < st.st_size; pos++) {
+			switch (passes[i].method) {
+				case PATTERN:
+					fputc(passes[i].pattern[pati], fp);
+					pati = (pati + 1) % 3;
+					break;
+
+				case RANDOM:
+				default:
+					fputc(rand(), fp);
+					break;
+			}
+		}
+
+		/* force a write to disk */
+		fflush(fp);
+		fsync(fd);
+		sync();
+	}
+
+	fclose(fp);
+
+	exit(EXIT_SUCCESS);
+}
