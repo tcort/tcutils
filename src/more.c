05b36bb2978bde68c0a4c11cd92766200c8210db
Thomas Cort <tcort@tcort.dev>
2022-03-12 00:04:42 +0000

    feat(more): add more utility

---
+++ src/more.c
@@ -0,0 +1,135 @@
+ /*
+    more -- filter for paging through text, one screen at a time
+    Copyright (C) 2022  Thomas Cort
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <errno.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+
+static struct termios old_termios;
+
+static void rawoff(void) {
+	 tcsetattr(STDIN_FILENO, TCSAFLUSH, &old_termios);
+}
+
+static void rawon(void) {
+	struct termios new_termios;
+
+	tcgetattr(STDIN_FILENO, &old_termios);
+	atexit(rawoff);
+
+	new_termios = old_termios;
+	new_termios.c_lflag &= ~(ECHO|ICANON);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &new_termios);
+}
+
+
+int main(int argc, char *argv[]) {
+
+	int ch;
+	size_t lineno;
+	FILE *f;
+	char *line = NULL;
+	size_t cap = 0;
+	ssize_t len = 0;
+
+	static struct option long_options[] = {
+		{ "help", no_argument, 0, 'h' },
+		{ "version", no_argument, 0, 'V' },
+		{ 0, 0, 0, 0 }
+	};
+
+	while ((ch = getopt_long(argc, argv, "hV", long_options, NULL)) != -1) {
+		switch (ch) {
+			case 'h':
+				fprintf(stdout, "more -- filter for paging through text, one screen at a time\n");
+				fprintf(stdout, "\n");
+				fprintf(stdout, "usage: more [OPTIONS] [FILE]\n");
+				fprintf(stdout, "\n");
+				fprintf(stdout, "  -h, --help     print help text\n");
+				fprintf(stdout, "  -V, --version  print version and copyright info\n");
+				fprintf(stdout, "\n");
+				fprintf(stdout, "examples:\n");
+				fprintf(stdout, "\n");
+				fprintf(stdout, "  # look at the listing of a large directory\n");
+				fprintf(stdout, "  ls | more\n");
+				exit(EXIT_SUCCESS);
+				break;
+			case 'V':
+				fprintf(stdout, "more (tcutils) v1.0.0\n");
+				fprintf(stdout, "Copyright (C) 2022  Thomas Cort\n");
+				fprintf(stdout, "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n");
+				fprintf(stdout, "This is free software: you are free to change and redistribute it.\n");
+				fprintf(stdout, "There is NO WARRANTY, to the extent permitted by law.\n");
+				fprintf(stdout, "\n");
+				fprintf(stdout, "Written by Thomas Cort.\n");
+				exit(EXIT_SUCCESS);
+				break;
+			default:
+				fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
+				exit(EXIT_FAILURE);
+				break;
+		}
+
+	}
+
+	argc -= optind;
+	argv += optind;
+
+
+	if (argc == 1) {
+		f = fopen(argv[0], "r");
+		if (f == NULL) {
+			perror("fopen");
+			exit(EXIT_FAILURE);
+		}
+	} else if (argc == 0) {
+		f = stdin;
+	} else {
+		fprintf(stdout, "usage: more [OPTIONS] [FILE]\n");
+		exit(EXIT_FAILURE);
+	}
+
+	rawon();
+
+	for (lineno = 0; !feof(f) && !ferror(f); lineno++) {
+		len = getline(&line, &cap, f);
+		if (len <= 0) {
+			break;
+		}
+
+		fprintf(stdout, "%s", line);
+
+		if (lineno % 24 == 23) {
+			char ch;
+			ch = getc(stdin);
+			if (ch == 'q' || ch == 'Q') {
+				break;
+			}
+		}
+	}
+
+	free(line);
+	fclose(f);
+
+	exit(EXIT_SUCCESS);
+}
