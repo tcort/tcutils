7092078df789050776aee675061ccb116597bcdb
Thomas Cort <tcort@tcort.dev>
2022-04-15 16:49:18 +0000

    refactor(echo): convert to libtc

--- src/echo.c
+++ src/echo.c
@@ -16,86 +16,76 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include "tc/args.h"
 #include "tc/const.h"
+#include "tc/stdio.h"
 #include "tc/sys.h"
 #include "tc/version.h"
 
-#include <getopt.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
 int main(int argc, char *argv[]) {
 
-	int ch;
 	int i;
 	int flag_n;
 
-	static struct option long_options[] = {
-		{ "help", no_argument, 0, 'h' },
-		{ "version", no_argument, 0, 'V' },
-		{ 0, 0, 0, 0 }
+	struct tc_prog_arg *arg;
+
+	static struct tc_prog_arg args[] = {
+		{ .arg = 'h', .longarg = "help", .description = "print help text", .has_value = 0 },
+		{ .arg = 'n', .longarg = "no-newline", .description = "omit trailing newline", .has_value = 0 },
+		{ .arg = 'V', .longarg = "version", .description = "print version and copyright info", .has_value = 0 },
+		TC_PROG_ARG_END
+	};
+
+	static struct tc_prog_example examples[] = {
+		{ .command = "echo \"Hello, ${USER}!\"", .description = "greet the user" },
+		{ .command = "echo -n \"What is your name\"", .description = "print a message without a newline" },
+		{ .command = "echo", .description = "print a newline" },
+		TC_PROG_EXAMPLE_END
+	};
+
+	static struct tc_prog prog = {
+		.program = "echo",
+		.usage = "[OPTIONS] [ARGS...]",
+		.description = "prints it's command line arguments",
+		.package = TC_VERSION_NAME,
+		.version = TC_VERSION_STRING,
+		.copyright = TC_VERSION_COPYRIGHT,
+		.license = TC_VERSION_LICENSE,
+		.author =  TC_VERSION_AUTHOR,
+		.args = args,
+		.examples = examples
 	};
 
 	/* defaults */
 	flag_n = 0;
 
-	while ((ch = getopt_long(argc, argv, "hnV", long_options, TC_NULL)) != -1) {
-		switch (ch) {
+	while ((arg = tc_args_process(&prog, argc, argv)) != TC_NULL) {
+		switch (arg->arg) {
 			case 'h':
-				fprintf(stdout, "echo -- prints it's command line arguments\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "usage: echo [OPTIONS] [ARG...]\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "  -n             omit trailing newline\n");
-				fprintf(stdout, "  -h, --help     print help text\n");
-				fprintf(stdout, "  -V, --version  print version and copyright info\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "examples:\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "  # greet the user\n");
-				fprintf(stdout, "  echo \"Hello, ${USER}!\"\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "  # print a message without a newline\n");
-				fprintf(stdout, "  echo -n \"What is your name?\"\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "  # print a newline\n");
-				fprintf(stdout, "  echo\n");
-				tc_exit(TC_EXIT_SUCCESS);
+				tc_args_show_help(&prog);
 				break;
 			case 'n':
 				flag_n = 1;
 				break;
 			case 'V':
-				fprintf(stdout, "echo (%s) v%s\n", TC_VERSION_NAME, TC_VERSION_STRING);
-				fprintf(stdout, "Copyright (C) 2022  Thomas Cort\n");
-				fprintf(stdout, "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n");
-				fprintf(stdout, "This is free software: you are free to change and redistribute it.\n");
-				fprintf(stdout, "There is NO WARRANTY, to the extent permitted by law.\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "Written by Thomas Cort.\n");
-				tc_exit(TC_EXIT_SUCCESS);
-				break;
-			default:
-				fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
-				tc_exit(TC_EXIT_FAILURE);
+				tc_args_show_version(&prog);
 				break;
 		}
 
 	}
 
-	argc -= optind;
-	argv += optind;
+	argc -= argi;
+	argv += argi;
 
 	for (i = 0; i < argc; i++) {
-		fprintf(stdout, "%s", argv[i]);
+		tc_puts(TC_STDOUT, argv[i]);
 		if (i + 1 < argc) {
-			fprintf(stdout, " ");
+			tc_puts(TC_STDOUT, " ");
 		}
 	}
 
 	if (flag_n == 0) {
-		fprintf(stdout, "\n");
+		tc_puts(TC_STDOUT, "\n");
 	}
 
 	tc_exit(TC_EXIT_SUCCESS);
