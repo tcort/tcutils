bd6bfc2b786640d5ab52d16a6ddf369fc1d891bf
Thomas Cort <tcort@tcort.dev>
2022-04-22 23:28:40 +0000

    refactor(string): tc_chomp/tc_chompd only last char

--- include/tc/string.h
+++ include/tc/string.h
@@ -31,8 +31,8 @@ char *tc_utoa(unsigned int n);
 int tc_atoi(char *s);
 int tc_strchr(char *s, char c);
 int tc_strrchr(char *s, char c);
-void tc_chomp(char *s);
-void tc_chompd(char *s, int delimiter);
+int tc_chomp(char *s);
+int tc_chompd(char *s, int delimiter);
 int tc_strlist_includes(char *haystack[], char *needle);
 
 #endif
