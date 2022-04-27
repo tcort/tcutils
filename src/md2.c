52ed7cc0991ce993ebe5e2d0aea75821e1b5e984
Thomas Cort <tcort@tcort.dev>
2022-04-27 09:49:43 +0000

    refactor(md2): getopt to libtc

--- src/md2.c
+++ src/md2.c
@@ -16,11 +16,11 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include "tc/args.h"
 #include "tc/const.h"
 #include "tc/sys.h"
 #include "tc/version.h"
 
-#include <getopt.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -194,51 +194,47 @@ int main(int argc, char *argv[]) {
 	byte *p, *q;
 	byte b;
 
-	static struct option long_options[] = {
-		{ "help", no_argument, 0, 'h' },
-		{ "version", no_argument, 0, 'V' },
-		{ 0, 0, 0, 0 }
+	struct tc_prog_arg *arg;
+
+	static struct tc_prog_arg args[] = {
+		{ .arg = 'h', .longarg = "help", .description = "print help text", .has_value = 0 },
+		{ .arg = 'V', .longarg = "version", .description = "print version and copyright info", .has_value = 0 },
+		TC_PROG_ARG_END
 	};
 
-	while ((ch = getopt_long(argc, argv, "hV", long_options, TC_NULL)) != -1) {
-		switch (ch) {
+	static struct tc_prog_example examples[] = {
+		{ .command = "md2 foo.txt", .description = "print the MD2 message digest of a file" },
+		{ .command = "... | md2", .description = "print the MD2 message digest of standard input" },
+		TC_PROG_EXAMPLE_END
+	};
+
+	static struct tc_prog prog = {
+		.program = "md2",
+		.usage = "[OPTIONS] [FILE]",
+		.description = "calculate a message-digest fingerprint for a file",
+		.package = TC_VERSION_NAME,
+		.version = TC_VERSION_STRING,
+		.copyright = TC_VERSION_COPYRIGHT,
+		.license = TC_VERSION_LICENSE,
+		.author =  TC_VERSION_AUTHOR,
+		.args = args,
+		.examples = examples
+	};
+
+	while ((arg = tc_args_process(&prog, argc, argv)) != TC_NULL) {
+		switch (arg->arg) {
 			case 'h':
-				fprintf(stdout, "md2 -- calculate a message-digest fingerprint for a file\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "usage: md2 [OPTIONS] [FILEn");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "  -h, --help     print help text\n");
-				fprintf(stdout, "  -V, --version  print version and copyright info\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "examples:\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "  # print the MD2 message digest of a file\n");
-				fprintf(stdout, "  md2 foo.txt\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "  # print the MD2 message digest of standard input\n");
-				fprintf(stdout, "  ... | md2\n");
-				tc_exit(TC_EXIT_SUCCESS);
+				tc_args_show_help(&prog);
 				break;
 			case 'V':
-				fprintf(stdout, "md2 (%s) v%s\n", TC_VERSION_NAME, TC_VERSION_STRING);
-				fprintf(stdout, "Copyright (C) 2022  Thomas Cort\n");
-				fprintf(stdout, "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n");
-				fprintf(stdout, "This is free software: you are free to change and redistribute it.\n");
-				fprintf(stdout, "There is NO WARRANTY, to the extent permitted by law.\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "Written by Thomas Cort.\n");
-				tc_exit(TC_EXIT_SUCCESS);
-				break;
-			default:
-				fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
-				tc_exit(TC_EXIT_FAILURE);
+				tc_args_show_version(&prog);
 				break;
 		}
 
 	}
 
-	argc -= optind;
-	argv += optind;
+	argc -= argi;
+	argv += argi;
 
 	fp = argc == 0 ? stdin : fopen(argv[0], "r");
 	if (fp == TC_NULL) {
@@ -276,7 +272,6 @@ int main(int argc, char *argv[]) {
 	free(p);
 
 	fclose(fp);
-
 
 	tc_exit(TC_EXIT_SUCCESS);
 }
