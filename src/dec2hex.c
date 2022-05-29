5bea3d5d67ce1a7b303054bce2815c38be1bec19
Thomas Cort <tcort@tcort.dev>
2022-05-29 10:14:41 +0000

    feat(dec2hex): add dec2hex utility

---
+++ src/dec2hex.c
@@ -0,0 +1,81 @@
+ /*
+    dec2hex -- convert decimal to hexadecimal
+    Copyright (C) 2022  Thomas Cort
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "tc/args.h"
+#include "tc/const.h"
+#include "tc/string.h"
+#include "tc/sys.h"
+#include "tc/version.h"
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+	int value;
+
+	struct tc_prog_arg *arg;
+
+	static struct tc_prog_arg args[] = {
+		TC_PROG_ARG_HELP,
+		TC_PROG_ARG_VERSION,
+		TC_PROG_ARG_END
+	};
+
+	static struct tc_prog_example examples[] = {
+		{ .command = "dec2hex 242", .description = "show the hexadecimal representation of 242" },
+		TC_PROG_EXAMPLE_END
+	};
+
+	static struct tc_prog prog = {
+		.program = "dec2hex",
+		.usage = "[OPTIONS] INTEGER",
+		.description = "convert decimal to hexadecimal",
+		.package = TC_VERSION_NAME,
+		.version = TC_VERSION_STRING,
+		.copyright = TC_VERSION_COPYRIGHT,
+		.license = TC_VERSION_LICENSE,
+		.author =  TC_VERSION_AUTHOR,
+		.args = args,
+		.examples = examples
+	};
+
+	while ((arg = tc_args_process(&prog, argc, argv)) != TC_NULL) {
+		switch (arg->arg) {
+			case 'h':
+				tc_args_show_help(&prog);
+				break;
+			case 'V':
+				tc_args_show_version(&prog);
+				break;
+		}
+
+	}
+
+	argc -= argi;
+	argv += argi;
+
+	if (argc != 1) {
+		tc_args_show_usage(&prog);
+		tc_exit(TC_EXIT_FAILURE);
+	}
+
+	value = tc_atoi(argv[0]);
+	fprintf(stdout, "%x\n", value);
+
+	tc_exit(TC_EXIT_SUCCESS);
+}
