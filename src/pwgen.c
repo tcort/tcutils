1235c275e5cdd50bea68d40251ff781b87ad6a6d
Thomas Cort <tcort@tcort.dev>
2022-10-09 11:10:10 +0000

    fix(pwgen): gcc 8.3.0 x86_64-linux-gnu compile error

--- src/pwgen.c
+++ src/pwgen.c
@@ -26,16 +26,7 @@
 #include "tc/version.h"
 
 static const int alphabet_size = 0x40;
-static const char alphabet[alphabet_size] = {
-	'A', 'B', 'C', 'D',  'E', 'F', 'G', 'H',
-	'I', 'J', 'K', 'L',  'M', 'N', 'O', 'P',
-	'Q', 'R', 'S', 'T',  'U', 'V', 'W', 'X',
-	'Y', 'Z', 'a', 'b',  'c', 'd', 'e', 'f',
-	'g', 'h', 'i', 'j',  'k', 'l', 'm', 'n',
-	'o', 'p', 'q', 'r',  's', 't', 'u', 'v',
-	'w', 'x', 'y', 'z',  '0', '1', '2', '3',
-	'4', '5', '6', '7',  '8', '9', '_', '-'
-};
+static const char const *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-";
 
 int main(int argc, char *argv[]) {
 
