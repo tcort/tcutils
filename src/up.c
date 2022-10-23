b37f5d097e84a8eba40642899ce94f7d78c18cc3
Thomas Cort <tcort@tcort.dev>
2022-10-23 06:27:15 -0400

    refactor(up): switch from getopt to args

--- src/up.c
+++ src/up.c
@@ -19,7 +19,6 @@
 #include <tc/tc.h>
 
 #include <curl/curl.h>
-#include <getopt.h>
 #include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -54,72 +53,63 @@ int main(int argc, char *argv[]) {
 	char *username;
 	char *password;
 
-	static struct option long_options[] = {
-		{ "help", no_argument, 0, 'h' },
-		{ "password", required_argument, 0, 'p' },
-		{ "username", required_argument, 0, 'u' },
-		{ "version", no_argument, 0, 'V' },
-		{ "verbose", no_argument, 0, 'v' },
-		{ 0, 0, 0, 0 }
+	struct tc_prog_arg *arg;
+
+	static struct tc_prog_arg args[] = {
+		TC_PROG_ARG_HELP,
+		{ .arg = 'p', .longarg = "password", .description = "set password", .has_value = 1 },
+		{ .arg = 'u', .longarg = "username", .description = "set username", .has_value = 1 },
+		TC_PROG_ARG_VERSION,
+		{ .arg = 'v', .longarg = "verbose", .description = "print verbose debugging details", .has_value = 0 },
+		TC_PROG_ARG_END
+	};
+
+	static struct tc_prog_example examples[] = {
+		{ .command = "up www.tcort.dev", .description = "get the status of https://www.tcort.dev/" },
+		{ .command = "up smtp://mail.tcort.dev", .description = "get the status of a mail server" },
+		TC_PROG_EXAMPLE_END
+	};
+
+	static struct tc_prog prog = {
+		.program = "up",
+		.usage = "[OPTIONS]",
+		.description = "universal ping, checks the status of a URL",
+		.package = TC_VERSION_NAME,
+		.version = TC_VERSION_STRING,
+		.copyright = TC_VERSION_COPYRIGHT,
+		.license = TC_VERSION_LICENSE,
+		.author =  TC_VERSION_AUTHOR,
+		.args = args,
+		.examples = examples
 	};
 
 	flag_v = 0;
 	username = TC_NULL;
 	password = TC_NULL;
 
-	while ((ch = getopt_long(argc, argv, "hu:p:Vv", long_options, TC_NULL)) != -1) {
-		switch (ch) {
+	while ((arg = tc_args_process(&prog, argc, argv)) != TC_NULL) {
+		switch (arg->arg) {
 			case 'h':
-				fprintf(stdout, "up -- universal ping, checks the status of a URL\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "Supported protocols: ftp, http, https, imap, imaps, scp, smtp, smtps\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "usage: up [OPTIONS] URL\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "  -h, --help            print help text\n");
-				fprintf(stdout, "  -p, --password=abc23  set password\n");
-				fprintf(stdout, "  -u, --username=alice  set username\n");
-				fprintf(stdout, "  -V, --version         print version and copyright info\n");
-				fprintf(stdout, "  -v, --verbose         print verbose debugging details\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "examples:\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "  # get the status of https://www.tcort.dev/\n");
-				fprintf(stdout, "  up www.tcort.dev\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "  # get the status of a mail server\n");
-				fprintf(stdout, "  up smtp://mail.tcort.dev\n");
-				tc_exit(TC_EXIT_SUCCESS);
+				tc_args_show_help(&prog);
 				break;
 			case 'V':
-				fprintf(stdout, "up (%s) v%s\n", TC_VERSION_NAME, TC_VERSION_STRING);
-				fprintf(stdout, "Copyright (C) 2022  Thomas Cort\n");
-				fprintf(stdout, "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n");
-				fprintf(stdout, "This is free software: you are free to change and redistribute it.\n");
-				fprintf(stdout, "There is NO WARRANTY, to the extent permitted by law.\n");
-				fprintf(stdout, "\n");
-				fprintf(stdout, "Written by Thomas Cort.\n");
-				tc_exit(TC_EXIT_SUCCESS);
+				tc_args_show_version(&prog);
 				break;
 			case 'v':
 				flag_v = 1;
 				break;
 			case 'u':
-				username = optarg;
+				username = argval;
 				break;
 			case 'p':
-				password = optarg;
-				break;
-			default:
-				fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
-				tc_exit(TC_EXIT_FAILURE);
+				password = argval;
 				break;
 		}
 
 	}
 
-	argc -= optind;
-	argv += optind;
+	argc -= argi;
+	argv += argi;
 
 	if (argc != 1) {
 		fprintf(stderr, "usage: up [OPTIONS] URL\n");
