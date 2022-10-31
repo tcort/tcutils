d0fee1bc0269ba27c89b7cdf10191279d6a4bf27
Thomas Cort <tcort@tcort.dev>
2022-10-31 04:26:21 -0400

    test(md2.check): additional test cases

--- tests/md2.check.c
+++ tests/md2.check.c
@@ -30,7 +30,13 @@ static int check_md2(void) {
 	char *md2;
 
 	struct testcase cases[] = {
-		{ "", "8350e5a3e24c153df2275c9f80692773" }
+		{ "", "8350e5a3e24c153df2275c9f80692773" },
+		{ "a", "32ec01ec4a6dac72c0ab96fb34c0b5d1" },
+		{ "abc", "da853b0d3f88d99b30283a69e6ded6bb" },
+		{ "message digest", "ab4f496bfb2a530b219ff33031fe06b0" },
+		{ "abcdefghijklmnopqrstuvwxyz", "4e8ddff3650292ab5a4108c3aa47940b" },
+		{ "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", "da33def2a42df13975352846c30338cd" },
+		{ "12345678901234567890123456789012345678901234567890123456789012345678901234567890", "d5976f79d83d3a0dc9806c3c66f3efd8" }
 	}; 
 
 	for (i = 0; i < sizeof(cases)/sizeof(cases[0]); i++) {
